9ad0bdf712f2ca41e3115c9f59c08cabb711c669
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-11-11 23:00:43 +0900

    TaskRepositoryの作成
    ・Taskモデルとの全アクセスロジックを持つ。
    ・アプリケーション全体からEloquentクエリを共有する必要が起きるようになると有効。
    ・特にアプリケーションが大きくなると有効。

---
+++ app/Repositories/TaskRepository.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Repositories;
+
+use App\User;
+
+class TaskRepository
+{
+    /**
+     * 指定ユーザーの全タスク取得
+     *
+     * @param  User  $user
+     * @return Collection
+     */
+    public function forUser(User $user)
+    {
+        return $user->tasks()
+                    ->orderBy('created_at', 'asc')
+                    ->get();
+    }
+}
+
+// 全データアクセスに対して利用するTaskRepositoryをLaravelの依存注入能力を使い、TaskControllerに注入する。？？？
+
+// TaskRepositoryを定義することで、Taskモデルとの全アクセスロジックを持つ。これは特にアプリケーションが大きくなり、アプリケーション全体からEloquentクエリを共有する必要が起きるようになると有効な手段。
