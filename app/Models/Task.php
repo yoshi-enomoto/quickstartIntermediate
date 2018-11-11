d99cb60b424d9696271c026b3d1bdf4ee94a7e2e
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-11-11 18:46:55 +0900

    2つもmodelへの定義
    ・taskテーブルへのfillable設定
    ・リレーション設定

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
 
 class Task extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/User.php
+++ app/User.php
@@ -5,6 +5,8 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use App\Models\Task;
+
 
 class User extends Authenticatable
 {
@@ -27,4 +29,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
 }
