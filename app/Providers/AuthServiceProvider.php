687fcbf578341c212acfbed086a5e2eaff2f2cd5
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-11-11 23:47:56 +0900

    TaskモデルとTaskPolicyの関連付け
    →Taskインスタンスに対するアクションを認可したい場合に、毎回使われるべきポリシーをLaravelに指定している。
    ＝アクションを実行する際のルールの制定？

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -9,11 +9,16 @@ class AuthServiceProvider extends ServiceProvider
 {
     /**
      * The policy mappings for the application.
+     * アプリケーションにマップするポリシー
      *
      * @var array
      */
     protected $policies = [
         'App\Model' => 'App\Policies\ModelPolicy',
+
+        'App\Task' => 'App\Policies\TaskPolicy',
+            // TaskモデルをTaskPolicyに関連付ける。
+            // Taskインスタンスに対するアクションを認可したい場合に毎回使われるべきポリシーをLaravelに指定することができる。
     ];
 
     /**
